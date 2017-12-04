40f8e384ce7e128e32d69b0e557bb7a38935e203
Edgar Nkongme <ed@bongkishiy.de>
2017-12-04 17:32:39 +0100

    Added cosmetic changes for cleaner code read

--- Server/CP/admin/picUpload/picUpload10.php
+++ Server/CP/admin/picUpload/picUpload10.php
@@ -52,7 +52,9 @@ if(isset($_POST["btn"]) && tokenIsValid($_POST['csrf_token'], 'picture-uploader'
     }
 
     try {
-        $info = getimagesize($_FILES["pic"]["tmp_name"]);
+        $tmp  = $_FILES["pic"]["tmp_name"];
+        $info = getimagesize($tmp);
+
         if ($info === FALSE) {
             throw new Exception("Unable to determine image type of uploaded file");
         }
@@ -61,21 +63,23 @@ if(isset($_POST["btn"]) && tokenIsValid($_POST['csrf_token'], 'picture-uploader'
             throw new Exception("Invalid file extension. Only JPG and JPEG allowed!");
         }
 
-        $tmp = $_FILES["pic"]["tmp_name"];
         if (!is_uploaded_file($tmp)) {
             throw new Exception("Unable to upload file!");
         }
 
         $fileName = sanitizeToAlphanumericDot($_FILES["pic"]["name"]);
         $location = "../shekayatPic/" . $fileName;
+
         if (!move_uploaded_file($tmp, $location)) {
             throw new Exception("Unable to move uploaded file to desired location!");
         }
 
-        $sql = "UPDATE `shekayatpic` SET `piclink` = ? WHERE `shekayatpic`.`id` = ?;";
+        $sql    = "UPDATE `shekayatpic` SET `piclink` = ? WHERE `shekayatpic`.`id` = ?;";
         $result = $connect->prepare($sql);
+
         $result->bindValue(1, $fileName, PDO::PARAM_STR);
         $result->bindValue(2, $_GET["id"], PDO::PARAM_INT);
+
         $query = $result->execute();
 
         if (!$query) {
@@ -83,49 +87,61 @@ if(isset($_POST["btn"]) && tokenIsValid($_POST['csrf_token'], 'picture-uploader'
         }
 
         if ($_GET["id"] == 1) {
-            $A = "shekayatHoghooghi.jpeg";
+            $A       = "shekayatHoghooghi.jpeg";
             $picNewA = "../shekayatPic/" . $A;
+
             rename($location, $picNewA);
+
             echo '<div class="posi chap">عکس مورد نظر شما ارسال شد</div>';
             echo "<br>";
             echo "<br>";
             echo '         <div class="posi chap"> <a href="../shekayatpic.php"><button>بازگشت</button></a></div>';
         } elseif ($_GET["id"] == 2) {
-            $B = "shekayatKeyfari.jpeg";
+            $B       = "shekayatKeyfari.jpeg";
             $picNewB = "../shekayatPic/" . $B;
+
             rename($location, $picNewB);
+
             echo '<div class="posi chap">عکس مورد نظر شما ارسال شد</div>';
             echo "<br>";
             echo "<br>";
             echo '         <div class="posi chap"> <a href="../shekayatpic.php"><button>بازگشت</button></a></div>';
         } elseif ($_GET["id"] == 3) {
-            $C = "shekayatBeDivanEdalat.jpeg";
+            $C       = "shekayatBeDivanEdalat.jpeg";
             $picNewC = "../shekayatPic/" . $C;
+
             rename($location, $picNewC);
+
             echo '<div class="posi chap">عکس مورد نظر شما ارسال شد</div>';
             echo "<br>";
             echo "<br>";
             echo '         <div class="posi chap"> <a href="../shekayatpic.php"><button>بازگشت</button></a></div>';
         } elseif ($_GET["id"] == 4) {
-            $D = "sheykayatBeSazmanBazresi.jpeg";
+            $D       = "sheykayatBeSazmanBazresi.jpeg";
             $picNewD = "../shekayatPic/" . $D;
+
             rename($location, $picNewD);
+
             echo '<div class="posi chap">عکس مورد نظر شما ارسال شد</div>';
             echo "<br>";
             echo "<br>";
             echo '         <div class="posi chap"> <a href="../shekayatpic.php"><button>بازگشت</button></a></div>';
         } elseif ($_GET["id"] == 5) {
-            $E = "shekayatBeSazmanMelal.jpeg";
+            $E       = "shekayatBeSazmanMelal.jpeg";
             $picNewE = "../shekayatPic/" . $E;
+
             rename($location, $picNewE);
+
             echo '<div class="posi chap">عکس مورد نظر شما ارسال شد</div>';
             echo "<br>";
             echo "<br>";
             echo '         <div class="posi chap"> <a href="../shekayatpic.php"><button>بازگشت</button></a></div>';
         } elseif ($_GET["id"] == 6) {
-            $F = "shekayatSayerMavared.jpeg";
+            $F       = "shekayatSayerMavared.jpeg";
             $picNewF = "../shekayatPic/" . $F;
+
             rename($location, $picNewF);
+
             echo '<div class="posi chap">عکس مورد نظر شما ارسال شد</div>';
             echo "<br>";
             echo "<br>";
